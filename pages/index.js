f558310f370951f7c848d3bfc45346752934df5d
hikaru <fyxstc@163.com>
2020-02-29 16:12:18 +0800

    jsprettier format

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,63 @@
 import Link from "next/link";
-import Router from 'next/router'
+import Router from "next/router";
 
 export default class Index extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: "",
-			content: ""
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      content: ""
+    };
+  }
 
-	handleTitleChange(e) {
-		this.setState({ title: e.target.value });
-	}
+  handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
 
-	handleContentChange(e) {
-		this.setState({ content: e.target.value });
-	}
+  handleContentChange(e) {
+    this.setState({ content: e.target.value });
+  }
 
-	handleSubmit() {
-		console.log(this.state.title);
-		console.log(this.state.content);
-		fetch("http://localhost:5000/texts", {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json"
-			},
-            body: JSON.stringify(this.state)
-		}).then(() => {
-            Router.push('/view')
-        });
-	}
+  handleSubmit() {
+    console.log(this.state.title);
+    console.log(this.state.content);
+    fetch("http://localhost:5000/texts", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }).then(() => {
+      Router.push("/view");
+    });
+  }
 
-	render() {
-		return (
-			<form>
-				<label>
-					title:
-					<br />
-					<input
-						type="text"
-						name="title"
-						onChange={this.handleTitleChange.bind(this)}
-					/>
-				</label>
-				<br />
-				content:
-				<br />
-				<textarea
-					rows="3"
-					cols="20"
-					onChange={this.handleContentChange.bind(this)}
-				></textarea>
-				<br />
-				<button type="button" onClick={this.handleSubmit.bind(this)}>
-					Submit
-				</button>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form>
+        <label>
+          title:
+          <br />
+          <input
+            type="text"
+            name="title"
+            onChange={this.handleTitleChange.bind(this)}
+          />
+        </label>
+        <br />
+        content:
+        <br />
+        <textarea
+          rows="3"
+          cols="20"
+          onChange={this.handleContentChange.bind(this)}
+        ></textarea>
+        <br />
+        <button type="button" onClick={this.handleSubmit.bind(this)}>
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
--- pages/view.js
+++ pages/view.js
@@ -1,37 +1,38 @@
 import Link from "next/link";
-import Router from 'next/router'
-
+import Router from "next/router";
 
 export default class View extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-        data:null,
+      data: null
     };
   }
   componentDidMount() {
-    fetch("http://localhost:5000/texts").then(resp => resp.json()).then(json => {
-      console.log(json) 
-      this.setState({data: json});
-    })
+    fetch("http://localhost:5000/texts")
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json);
+        this.setState({ data: json });
+      });
   }
 
-  render(){
-    if(!this.state.data){
-        return <p> loading </p>
-    }else{
-        return(
-            this.state.data.map((data) =>
-              <li key={data.tid}>{data.title} 
+  render() {
+    if (!this.state.data) {
+      return <p> loading </p>;
+    } else {
+      return this.state.data.map(data => (
+        <li key={data.tid}>
+          {data.title}
 
-              <Link href={'/edit?id=' + data.tid}>
-              <button type="button" value={data.tid}> edit </button>
-              </Link>
-              </li>
-            )
-        );
-
+          <Link href={"/edit?id=" + data.tid}>
+            <button type="button" value={data.tid}>
+              {" "}
+              edit{" "}
+            </button>
+          </Link>
+        </li>
+      ));
     }
   }
-
 }
